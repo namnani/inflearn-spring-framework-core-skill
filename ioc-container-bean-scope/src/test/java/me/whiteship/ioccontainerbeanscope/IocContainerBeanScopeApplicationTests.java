5a9be3f3d3bd566bd4793e80b7a5fd70957bc54a
younannam <younan.nam@worksmobile.com>
2020-08-30 14:55:43 +0900

    [Spring Framework] IoC Container - Bean Scope.  - singleton, prototype.  - 프로토타입 빈이 싱글톤 빈을 참조하면? 아무 문제없음.  - 싱글톤 빈이 프로토타입 빈을 참조하면? 프로토타입 빈이 업데이트가 안된다. scoped-proxy, object-provider등의 방법이 필요.  - 싱글톤 객체 사용 시 주의 점.    1. 프로퍼티가 공유.    2. ApplicationContext 초기 구동시 인스턴스 생성.

---
+++ ioc-container-bean-scope/src/test/java/me/whiteship/ioccontainerbeanscope/IocContainerBeanScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.ioccontainerbeanscope;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocContainerBeanScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
