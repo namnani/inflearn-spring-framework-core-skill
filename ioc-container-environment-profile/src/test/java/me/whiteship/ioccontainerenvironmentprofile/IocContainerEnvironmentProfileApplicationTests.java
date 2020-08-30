b543a1cb7e4be48c8e59857026cd0e085b42b8ce
younannam <younan.nam@worksmobile.com>
2020-08-30 15:27:50 +0900

    [Spring Framework] IoC Container - Environment.  1. Profile : 환경별로 정의된 빈들의 묶음.

---
+++ ioc-container-environment-profile/src/test/java/me/whiteship/ioccontainerenvironmentprofile/IocContainerEnvironmentProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.ioccontainerenvironmentprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocContainerEnvironmentProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
