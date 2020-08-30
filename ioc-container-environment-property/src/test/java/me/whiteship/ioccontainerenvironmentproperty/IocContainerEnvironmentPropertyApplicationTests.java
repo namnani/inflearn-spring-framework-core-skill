24c185dc29436471559459031df1d343064f10ea
younannam <younan.nam@worksmobile.com>
2020-08-30 15:34:15 +0900

    [Spring Framework] IoC Container - Environment.  2. Property : 애플리케이션에 등록된 key-value 쌍의 프로퍼티에 계층적으로 접근함.

---
+++ ioc-container-environment-property/src/test/java/me/whiteship/ioccontainerenvironmentproperty/IocContainerEnvironmentPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.ioccontainerenvironmentproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocContainerEnvironmentPropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
